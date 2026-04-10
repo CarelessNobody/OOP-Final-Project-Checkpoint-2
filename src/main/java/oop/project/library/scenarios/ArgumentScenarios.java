5408e1a1b7b1955840e23bac5df3fb6f987041b7
Ephraim Nicolas <ephraim1588@gmail.com>
2026-04-09 23:30:08 -0400

    (CODE ONLY) Added date scenario to ArgumentScenarios for reference.

--- src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
+++ src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
@@ -3,6 +3,12 @@ package oop.project.library.scenarios;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import oop.project.library.argument.ParseArgType;
+import oop.project.library.command.CommParsers;
+import oop.project.library.command.CommSpec;
+import oop.project.library.command.ParsedComm;
+import oop.project.library.input.BasicArgs;
+import oop.project.library.input.Input;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -27,7 +33,17 @@ public final class ArgumentScenarios {
     }
 
     public static Map<String, Object> date(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        try {
+            BasicArgs args = new Input(arguments).parseBasicArgs();
+
+            CommSpec spec = new CommSpec("date")
+                    .addPositional("date", s -> ParseArgType.parse(s, LocalDate::parse));
+
+            ParsedComm result = CommParsers.parsePositional(spec, args);
+            return result.asMap();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Invalid date command", e);
+        }
     }
 
 }
