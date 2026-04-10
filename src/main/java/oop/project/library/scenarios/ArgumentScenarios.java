537829d2f6a1ec4f1dcfa77386857192b6706e38
weng <weng2019157365@gmail.com>
2026-04-09 23:42:54 -0400

    Did the rest of the Argument Scenarios, but unsure if test case with negative doubles should be failing

--- src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
+++ src/main/java/oop/project/library/scenarios/ArgumentScenarios.java
@@ -12,24 +12,71 @@ import oop.project.library.input.Input;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Map;
 
 public final class ArgumentScenarios {
 
     public static Map<String, Object> add(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        try {
+            BasicArgs args = new Input(arguments).parseBasicArgs();
+
+            CommSpec spec = new CommSpec("add")
+                    .addPositional("left", ParseArgType::parseInt)
+                    .addPositional("right", ParseArgType::parseInt);
+
+            ParsedComm result = CommParsers.parsePositional(spec, args);
+            return result.asMap();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("invalid add command", e);
+        }
     }
 
     public static Map<String, Object> sub(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        try {
+            BasicArgs args = new Input(arguments).parseBasicArgs();
+
+            CommSpec spec = new CommSpec("sub")
+                    .addPositional("left", ParseArgType::parseDouble)
+                    .addPositional("right", ParseArgType::parseDouble);
+
+            ParsedComm result = CommParsers.parsePositional(spec, args);
+            return result.asMap();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("invalid sub command", e);
+        }
     }
 
     public static Map<String, Object> fizzbuzz(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        try {
+            BasicArgs args = new Input(arguments).parseBasicArgs();
+
+            CommSpec spec = new CommSpec("fizzbuzz")
+                    .addPositional("number", s ->
+                            ParseArgType.parse(s, Integer::parseInt, 1, 100
+                            ));
+
+            ParsedComm result = CommParsers.parsePositional(spec, args);
+            return result.asMap();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("invalid sub command", e);
+        }
     }
 
     public static Map<String, Object> difficulty(String arguments) throws RuntimeException {
-        throw new UnsupportedOperationException("TODO (PoC)");
+        try {
+            BasicArgs args = new Input(arguments).parseBasicArgs();
+
+            CommSpec spec = new CommSpec("difficulty")
+                    .addPositional("difficulty", s ->
+                                ParseArgType.parse(s, String::valueOf, List.of("easy", "normal", "hard", "peaceful")
+                            ));
+
+            ParsedComm result = CommParsers.parsePositional(spec, args);
+            return result.asMap();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("invalid sub command", e);
+        }
     }
 
     public static Map<String, Object> date(String arguments) throws RuntimeException {
